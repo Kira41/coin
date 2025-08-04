57bd394c95431394fffc73176bf68ee1678b644e
Kira41 <41kira41@gmail.com>
2025-08-04 09:39:37 -0400

    Delete run_cron_jobs.bat

--- run_cron_jobs.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Run all cron jobs for the Coin Dashboard project in an infinite loop
-cd /d "%~dp0"
-
-:loop
-php cron\cron_process_orders.php
-php cron\update_open_trades.php
-timeout /t 3 >nul
-goto loop
