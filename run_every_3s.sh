6128455cc533de3d0597f7e8dbedd9a263a19742
Kira41 <41kira41@gmail.com>
2025-08-04 09:39:44 -0400

    Delete run_every_3s.sh

--- run_every_3s.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {1..20}
-do
-    php /home/admin/web/c-trade.ca/public_html/cron/cron_process_orders.php
-    php /home/admin/web/c-trade.ca/public_html/cron/update_open_trades.php
-    sleep 3
-done
